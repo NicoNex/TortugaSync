9501c004b7bc283f3cf1386fe07e00fca5fe39d8
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2024-04-04 01:53:21 +0200

    Add styling to the file buttons.

--- cmd/kraken/main.go
+++ cmd/kraken/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"embed"
 )
+
+//go:embed template.html
+var templHTML embed.FS
 
 func main() {
 	var port string
@@ -14,12 +21,35 @@ func main() {
 	flag.StringVar(&port, "p", ":8085", "Specify the port to use.")
 	flag.Parse()
 
+	if !strings.HasPrefix(port, ":") {
+		port = ":" + port
+	}
+
 	home, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	t, err := template.ParseFS(templHTML, "template.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	path := filepath.Join(home, "bookmarks")
-	http.Handle("/", http.FileServer(http.Dir(path)))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		files, err := os.ReadDir(path)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		if err = t.Execute(w, files); err != nil {
+			log.Println(err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	})
+	http.Handle("/file/", http.StripPrefix("/file/", http.FileServer(http.Dir(path))))
 	log.Println(http.ListenAndServe(port, nil))
 }
